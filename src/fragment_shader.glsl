b344f5639ffd5469f0ccadc1d7456cbcc5bcdf1b
Yoshy <graygolem@gmail.com>
2018-03-03 21:07:33 +0500

    Modified shaders arrived

--- src/fragment_shader.glsl
+++ src/fragment_shader.glsl
@@ -1,10 +1,15 @@
 #version 330 core
 
 in vec3 vColor;
+in vec2 texCoords;
+in float mixA;
 
 out vec4 color;
 
+uniform sampler2D curTexture1;
+uniform sampler2D curTexture2;
+
 void main()
 {
-	color = vec4(vColor.r, vColor.g, vColor.b, 1.0);
+	color = mix(texture(curTexture1, texCoords), texture(curTexture2, texCoords), mixA) * vec4(vColor, 1.0);
 }
--- src/vertex_shader.glsl
+++ src/vertex_shader.glsl
@@ -2,8 +2,11 @@
 
 layout(location = 0) in vec3 pos;
 layout (location = 1) in vec3 color;
+layout (location = 2) in vec2 inTexCoords;
 
 out vec3 vColor;
+out vec2 texCoords;
+out float mixA;
 
 uniform vec3 shift;
 
@@ -11,4 +14,6 @@ void main()
 {
 	gl_Position = vec4(pos.x + shift.x, pos.y + shift.y, pos.z + shift.z, 1.0);
 	vColor = color;
+	texCoords = inTexCoords;
+	mixA = shift.x + 0.5;
 }
